a85fa44bf987d8484b2088a7930db1d8576a3a33
Anshul Bansal <anshul2005@gmail.com>
2022-11-01 20:13:22 -0400

    first file

---
+++ Begin.py
@@ -0,0 +1,11 @@
+# print ("hello world")
+Name = input("what is your name")
+Neighbours = input("what is your neighbour's name")
+Codingtime = int(input("how long have you been coding"))
+NeighbourCodingTime = int(input("how long have your neighbour been coding"))
+
+totaltime = Codingtime+NeighbourCodingTime
+
+print("my name is " + Name)
+print("my neighbour's name is " + Neighbours)
+print("me and neighbour have been coding together for", totaltime)
