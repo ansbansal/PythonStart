6c9a4f2d4a59b54f6c9cd34a84ba208e271eb54f
Anshul Bansal <anshul2005@gmail.com>
2022-11-01 21:14:00 -0400

    comit begin

--- Begin.py
+++ Begin.py
@@ -9,3 +9,23 @@ totaltime = Codingtime+NeighbourCodingTime
 print("my name is " + Name)
 print("my neighbour's name is " + Neighbours)
 print("me and neighbour have been coding together for", totaltime)
+
+
+# if loops
+x=5
+y=15
+if x==1:
+    print("x is equal to 1")
+elif x>5:
+    if y>10:
+        print("x is greater than 5 and y is greater than 10")
+    else:
+        print("x is greater than 5 and y is less than or equal to 10")
+elif y>10:
+    print("x is less than or equal to 5 and y is greater than 10")
+else:
+    print("x is less than or equal to 5 and y is less than or equal to 10")
+
+
+
+
